fdfa108d2fe91540122e733f2c83bd9c49f1e56b
Stephanny Barreto <stephbarreto@me.com>
2018-01-04 23:25:10 -0400

    up

---
+++ produto.js
@@ -0,0 +1,6 @@
+var inputTamanho = document.querySelector('[name=tamanho]')
+var outputTamanho = document.querySelector('[name=valortamanho]')
+
+￼function mostraTamanho(){
+￼	outputTamanho.value = inputTamanho.value
+}
--- produto.php
+++ produto.php
@@ -19,26 +19,29 @@
 		<h1>Fuzzy Cardigan</h1>
 		<p>por apenas R$ 129,00</p>
 	
-		<form>
-		<fieldset class="cores">
-			<legend>Escolha a cor:</legend>
-			
-			<input type="radio" name="cor" value="verde" id="verde" checked>
-			<label for="verde">
-				<img src="img/produtos/foto2-verde.png" alt="verde">
-			</label>
-			
-			<input type="radio" name="cor" value="rosa" id="rosa" checked>
-			<label for="rosa">
-				<img src="img/produtos/foto2-rosa.png" alt="rosa">
-			</label>
-			<input type="radio" name="cor" value="azul" id="azul" checked>
-			<label for="azul">
-				<img src="img/produtos/foto2-azul.png" alt="azul">
-			</label>
+		<form action="checkout.php" method="POST">
+			<input type="hidden" name="nome" value="Fuzzy Cardigan">
+			<input type="hidden" name="preco" value="129,00">
+			<fieldset class="cores">
+				<legend>Escolha a cor:</legend>
+				
+				<input type="radio" name="cor" value="verde" id="verde" checked>
+				<label for="verde">
+					<img src="img/produtos/foto2-verde.png" alt="verde">
+				</label>
+				
+				<input type="radio" name="cor" value="rosa" id="rosa" checked>
+				<label for="rosa">
+					<img src="img/produtos/foto2-rosa.png" alt="rosa">
+				</label>
+				<input type="radio" name="cor" value="azul" id="azul" checked>
+				<label for="azul">
+					<img src="img/produtos/foto2-azul.png" alt="azul">
+				</label>
 			<fieldset class="tamanhos">
 				<legend>Escolha o tamanho:</legend>
 				<input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
+				<output for="tamanho" name="valortamanho">42</output>
 			     </fieldset>
 			</fieldset>
 			<input type="submit" class="comprar" value="Comprar">
